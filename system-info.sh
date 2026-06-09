00e9b8a0c1ccc58926e18389c7df0a83a88ce2dc
dessymama <visit2dessy@gmail.com>
2026-06-09 15:34:29 +0100

    Day 3: Bash scripting - system info script

---
+++ system-info.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "===== SYSTEM INFO ====="
+
+echo "Date:"
+date
+
+echo ""
+
+echo "Who am I:"
+whoami
+
+echo ""
+
+echo "Current Folder:"
+pwd
+
+echo ""
+
+echo "Disk Usage:"
+df -h
+
+echo "======================="
